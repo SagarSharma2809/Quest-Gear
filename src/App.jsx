ad80466e66c1ef7b753db1a71ec36c22bd3a90f2
Sagar Sharma <sagarsharma2809@gmail.com>
2024-08-02 14:10:25 +0530

    set up finished

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
 
   return (
     <>
-      <h1 className='text-3xl font-bold underline'>ForestV</h1>
+      <h1 className='text-3xl font-bold underline'>QUEST GEAR</h1>
     </>
   )
 }
