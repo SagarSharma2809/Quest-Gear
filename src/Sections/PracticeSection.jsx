41ee1891ea86d7db2488c3bfd5c0a52570a97d40
Sagar Sharma <sagarsharma2809@gmail.com>
2024-08-12 21:18:47 +0530

    character set up in practice section

--- src/Sections/PracticeSection.jsx
+++ src/Sections/PracticeSection.jsx
@@ -1,24 +1,35 @@
+
+
+
 export default function PracticeSection({ charName }) {
 
     const charStats = {
         Knight: {
             emoji: "⚔️",
-            hearts: "❤️❤️❤️❤️❤️"
+            hearts: "❤️❤️❤️❤️❤️",
+            idle: '/Img/Knight Idle.gif',
+            width: "2/3"
         },
 
         Archer: {
             emoji: "🏹",
-            hearts: "❤️❤️❤️"
+            hearts: "❤️❤️❤️",
+            idle: '/Img/Archer Idle.gif',
+            width: "1/2"
         },
 
         Mage: {
             emoji: "📖",
-            hearts: "❤️❤️"
+            hearts: "❤️❤️",
+            idle: '/Img/Mage Idle.gif',
+            width: "1/2"
         },
 
         Rogue: {
             emoji: "🔪",
-            hearts: "❤️❤️❤️❤️"
+            hearts: "❤️❤️❤️❤️",
+            idle: "Img/Rogue Idle.gif",
+            width: "1/2"
         }
     }
 
@@ -26,11 +37,19 @@ export default function PracticeSection({ charName }) {
         <div className="p-4 bg-blue-500 h-screen">
             <h1 className="text-5xl text-center text-white">Practice Zone</h1>
 
-            <div className="bg-white w-1/4 p-4 flex items-center">
+            <div className="w-1/5 p-4 text-white flex flex-col">
 
-                <h2 className="text-xl font-bold">{charName.toUpperCase()} {charStats[charName].emoji} {charStats[charName].hearts}</h2>
+                <div className="text-xl font-bold flex items-center">
+                    <div>{charName.toUpperCase()}</div>
+                    <div>{charStats[charName].emoji}</div>
+                    <div>{charStats[charName].hearts}</div>
+                </div>
+                <img src={charStats[charName].idle} alt="" className={`w-${charStats[charName].width} my-4`} />
 
             </div>
+
+
+
 
         </div>
     )
