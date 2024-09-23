7037acab609f327f4731ba9124a0c8c75b318ab5
Sagar Sharma <sagarsharma2809@gmail.com>
2024-09-23 22:23:06 +0530

    testing 2

--- src/pages/HeroPage.tsx
+++ src/pages/HeroPage.tsx
@@ -80,7 +80,7 @@ export default function HeroPage() {
 
             <div className="py-4" style={contentStyles}>
                 <div className="m-10 text-center">
-                    <Heading text="Choose your Character, hi" />
+                    <Heading text="Choose your Character" />
                 </div>
 
 
